5f602c596473d404c2cbe547cc0f19719f64256b
Ariel <ariel32_124@hotmail.com>
2023-06-29 15:55:05 -0600

    Sección ourWork para desktop

--- src/pages/trabajo/index.jsx
+++ src/pages/trabajo/index.jsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/navbar";
+import OurWork from "@/components/ourWork/OurWork";
 
 export default function Trabajo() {
     return (
       <>
         <Navbar />
         <main>
-          
+          <OurWork/>
         </main>
       </>
     );
