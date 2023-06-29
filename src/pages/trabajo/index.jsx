864fe7c488402f40454a16bb6d547a654f9020d2
ailedviv <ailed@pixabyte.com>
2023-06-29 10:24:41 -0600

    new page work

---
+++ src/pages/trabajo/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+
+export default function Trabajo() {
+    return (
+      <>
+        <Navbar />
+        <main>
+          
+        </main>
+      </>
+    );
+}
