877e02e601957b3b1da70909965d4809f5f677e5
Luis.Miranda <luis.miranda@pixabyte.team>
2023-06-29 15:23:18 -0600

    add/testimonials

--- src/pages/trabajo/index.jsx
+++ src/pages/trabajo/index.jsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/navbar";
+import Testimonials from "@/components/testimonials";
 
 export default function Trabajo() {
     return (
       <>
         <Navbar />
         <main>
-          
+          <Testimonials />
         </main>
       </>
     );
