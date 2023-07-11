746351947c03731414bce4f00f0714b2243a846e
Carmona-BYTE <alejandro.carmona@pixabyte.team>
2023-07-11 09:36:15 -0600

    Cambios minimos en heaer y banner

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,18 +1,20 @@
-import "../../app/globals.scss"
-import style from "./Banner.module.scss"
+import "../../app/globals.scss";
+import style from "./Banner.module.scss";
 
 export default function Banner() {
   return (
     <section className={style.container}>
-      <p className={style.title}>
-        <span>T</span>U SOCIO EN TECNOLOGÍA
-      </p>
-      <p className={style.description}>
-        Maximiza tus resultados con tecnología de punta
-      </p>
-      <button type="submit" className={style.btnContactUs}>
-        Contáctanos
-      </button>
+      <div className={style.content}>
+        <p className={style.title}>
+          <span>T</span>U SOCIO EN TECNOLOGÍA
+        </p>
+        <p className={style.description}>
+          Maximiza tus resultados con tecnología de punta
+        </p>
+        <button type="submit" className={style.btnContactUs}>
+          Contáctanos
+        </button>
+      </div>
     </section>
   );
 }
