83992912aaf1b4b83f2b29501e7e796b57dca403
SergioCMz <sergio5251949@gmail.com>
2023-07-29 00:09:27 -0600

    Footer responsive

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,3 +1,4 @@
+// index.jsx
 import React from "react";
 import "../../app/globals.scss";
 import style from "./Footer.module.scss";
@@ -8,31 +9,43 @@ import icontwitter from "@/assets/icons/icon-twitter.png";
 import iconfacebook from "@/assets/icons/icon-facebook.png";
 
 export default function Footer() {
-    return (
-      <footer className={style.footer}>
-        <div className={style.leftsection}>
-          <Image src={logotype} alt="Albbus" className={style.logo} />
-          <p className={style.rights}>Todos los derechos reservados Albbus 2023</p>
+  return (
+    <footer className={style.footer}>
+      <div className={style.leftsection}>
+        <Image src={logotype} alt="Albbus" className={style.logo} />
+        <p className={style.rights}>Todos los derechos reservados Albbus 2023</p>
+      </div>
+      <div className={style.rightsection}>
+        <p className={style.follow}>Síguenos</p>
+        <div className={style.socialicons}>
+          <a href="https://www.linkedin.com/company/pixabytemx">
+            <Image src={iconlinkedin} alt="LinkedIn" className={style.icon} />
+          </a>
+          <a href="https://twitter.com/Pixabyte_?t=nSxTqJ-yPUzfIWH0mItaOw&s=08">
+            <Image src={icontwitter} alt="Twitter" className={style.icon} />
+          </a>
+          <a href="https://www.facebook.com/PixabyteL?mibextid=ZbWKw">
+            <Image src={iconfacebook} alt="Facebook" className={style.icon} />
+          </a>
         </div>
-        <div className={style.rightsection}>
-          <p className={style.follow}>Síguenos</p>
-          <div className={style.socialicons}>
-            <a href="https://www.linkedin.com/company/pixabytemx">
-              <Image src={iconlinkedin} alt="LinkedIn" className={style.icon} />
+        <div className={style.linkssection}>
+          <p>
+            <a href="/aviso-privacidad" className={style.linksfooter}>
+              Politica de privacidad
             </a>
-            <a href="https://twitter.com/Pixabyte_?t=nSxTqJ-yPUzfIWH0mItaOw&s=08">
-              <Image src={icontwitter} alt="Twitter" className={style.icon} />
+          </p>
+          <p>
+            <a href="/terminos-condiciones" className={style.linksfooter}>
+              Términos y condiciones
             </a>
-            <a href="https://www.facebook.com/PixabyteL?mibextid=ZbWKw">
-              <Image src={iconfacebook} alt="Facebook" className={style.icon} />
+          </p>
+          <p>
+            <a href="#" className={style.linksfooter}>
+              Políticas de cookies
             </a>
-          </div>
-          <div className={style.linkssection}>
-            <p>Política de privacidad</p>
-            <p>Términos y condiciones</p>
-            <p>Políticas de cookies</p>
-          </div> 
+          </p>
         </div>
-      </footer>
-    );
-  };
+      </div>
+    </footer>
+  );
+};
