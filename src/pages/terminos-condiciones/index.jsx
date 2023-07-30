75856b6497866933094efc4ce26a94dc50b51f65
ailedviv <ailed@pixabyte.com>
2023-07-30 15:28:00 -0600

    correct rute file

--- src/pages/terminos-condiciones/index.jsx
+++ src/pages/terminos-condiciones/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../app/globals.scss";
 import style from "./Terminos.module.scss";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 export default function Aviso(){
 
