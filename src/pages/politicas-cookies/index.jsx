ef09d1dec8b38df016612e17448670a1187c20dd
ailedviv <ailed@pixabyte.com>
2023-08-04 18:40:36 -0600

    edit rute footer

--- src/pages/politicas-cookies/index.jsx
+++ src/pages/politicas-cookies/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../app/globals.scss";
 import style from "./Politicas.module.scss";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 export default function Cookies(){
 
