d1dfce4f6b4d7d4f594f6b305fba902584c1275b
ailedviv <ailed@pixabyte.com>
2023-08-11 09:11:38 -0600

    add function handleChangeInput

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -12,87 +12,89 @@ export default function Formcontact() {
     empresa: ''
   });
 
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value 
+    });
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
-      
       const response = await axios.post('URL_DE_TU_API', formData);
-
-    
       console.log('Respuesta de la solicitud:', response.data);
     } catch (error) {
-     
       console.error('Error al realizar la solicitud:', error);
     }
   };
-}
 
-
-export default function Formcontact() {
   return (
     <section className={style.containerprincipal}>
-    <div className={style.container}>
-      <h1 className={style.title}>Contáctanos</h1>
-      <hr className={style.line}></hr>
-      <p className={style.description}>¿Qué quieres solucionar?</p>
-      <form>
-      <input
-        className={style.input1}
-        type="text"
-        onChange={handleInputChange}
-        placeholder=""
-      />
-      <div className={style.inputform}>
-        <div className={style.column}>
-          <p>Nombre</p>
+      <div className={style.container}>
+        <h1 className={style.title}>Contáctanos</h1>
+        <hr className={style.line}></hr>
+        <p className={style.description}>¿Qué quieres solucionar?</p>
+        <form>
           <input
-            className={style.input}
+            className={style.input1}
             type="text"
-            onChange={handleInputChange}
-            placeholder="Ingresa tu nombre"
+            placeholder=""
           />
-        </div>
-        <div className={style.column}>
-          <p>Apellido</p>
-          <input
-            className={style.input}
-            type="text"
-            onChange={handleInputChange}
-            placeholder="Ingresa tu apellido"
-          />
-        </div>
-      </div>
-      <div className={style.inputform}>
-        <div className={style.column}>
-          <p>E-mail</p>
-          <input
-            className={style.input}
-            type="text"
-            onChange={handleInputChange}
-            placeholder="Ingresa tu nombre"
-          />
-        </div>
-        <div className={style.column}>
-          <p>Empresa</p>
-          <input
-            className={style.input}
-            type="text"
-            onChange={handleInputChange}
-            placeholder="Ingresa el nombre"
-          />
-        </div>
-      </div>
-      <p className={style.num}>Número Celular</p>
-      <div className={style.inputRow}>
-        <span className={style.Inputlada}>
-        <PhoneNumberInput/>
-        </span>
-      </div>
-      </form>
-      <button className={style.enviar} type="button">
-        Enviar
-      </button>
+          <div className={style.inputform}>
+            <div className={style.column}>
+              <p>Nombre</p>
+              <input
+                className={style.input}
+                type="text"
+                name="nombre"
+                onChange={handleInputChange}
+                placeholder="Ingresa tu nombre"
+              />
+            </div>
+            <div className={style.column}>
+              <p>Apellido</p>
+              <input
+                className={style.input}
+                type="text"
+                name="apellido"
+                onChange={handleInputChange}
+                placeholder="Ingresa tu apellido"
+              />
+            </div>
+          </div>
+          <div className={style.inputform}>
+            <div className={style.column}>
+              <p>E-mail</p>
+              <input
+                className={style.input}
+                type="text"
+                name="email"
+                onChange={handleInputChange}
+                placeholder="Ingresa tu correo"
+              />
+            </div>
+            <div className={style.column}>
+              <p>Empresa</p>
+              <input
+                className={style.input}
+                type="text"
+                name="empresa"
+                onChange={handleInputChange}
+                placeholder="Ingresa la empresa"
+              />
+            </div>
+          </div>
+          <p className={style.num}>Número Celular</p>
+          <div className={style.inputRow}>
+            <span className={style.Inputlada}>
+            <PhoneNumberInput/>
+            </span>
+          </div>
+        </form>
+        <button className={style.enviar} type="button">
+          Enviar
+        </button>
       </div>
     </section>
   );
