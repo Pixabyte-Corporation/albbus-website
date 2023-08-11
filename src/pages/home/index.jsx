fd4aa96cde4e21f92ea5e413da68cc042b8cdb9d
ailedviv <ailed@pixabyte.com>
2023-08-11 09:54:25 -0600

    update name files

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 import Banner from "@/components/Banner";
 import Description from "@/components/Description";
 import IconsBar from "@/components/IconsBar";
-import Carateristicas from "@/components/Caracteristicas";
+import Carateristicas from "@/components/caracteristicas";
 import Contact from "@/components/contact";
 import Footer from "@/components/Footer";
 
